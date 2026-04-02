e65d1df1895ee3890b1d8c4ff5de315e3faca053
Arnam Chaurasiya <arnam.chaurasiya@mphasis.com>
2026-04-02 14:52:42 +0530

    spring boot thyme lead=f using h2 database

---
+++ SpringBootThymeleaf_CRUD_H2DB/src/main/java/com/test/controller/EmployeeController.java
@@ -0,0 +1,94 @@
+package com.test.controller;
+
+import javax.xml.crypto.dsig.SignatureMethod;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.test.entity.Employee;
+import com.test.repository.EmployeeRepository;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/employees/")
+public class EmployeeController {
+	
+	private final EmployeeRepository employeeRepository;
+	
+	@Autowired
+	public EmployeeController(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+	
+	@GetMapping("signup")
+	public String showSignUpForm(Employee employee) {
+		return "add-employee";
+	}
+	
+	@GetMapping("/list")
+	public String showUpdateForm(Model model) {
+		model.addAttribute("employees", employeeRepository.findAll());
+		
+		return "index";
+	}
+	
+	
+	@PostMapping("add")
+	public String addEmployee(@Valid Employee employee, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) {
+			return "add-employees";
+		}
+		employeeRepository.save(employee);
+		return "redirect:list";
+	}
+	
+	@GetMapping("edit/{id}")
+	public String showUpdateForm(@PathVariable long id, Model model) {
+		
+		Employee employee = employeeRepository.findById(id).orElseThrow(
+				()-> new IllegalArgumentException("invalid employee id : "+id));
+				model.addAttribute("employee",employee);
+				return "update-employee";
+	}
+
+
+	@PostMapping("update/{id}")
+	public String updateEmployee(@PathVariable long id,
+	                             @Valid Employee employee,
+	                             BindingResult result,
+	                             Model model) {
+		
+	
+	if(result.hasErrors()) {
+		employee.setId(id);
+		
+		return "update-employees";
+	}
+	employeeRepository.save(employee);
+	
+	model.addAttribute("employees",employeeRepository.findAll());
+	
+	return "index";
+}
+	@GetMapping("delete/{id}")
+	public String deleteEmployee(@PathVariable long id, Model model) {
+	    Employee employee = employeeRepository.findById(id)
+	        .orElseThrow(() -> new IllegalArgumentException("Invalid employee id: " + id));
+
+	    employeeRepository.delete(employee);
+	    model.addAttribute("employees", employeeRepository.findAll());
+	    return "index";
+	}
+
+	}
+
+
